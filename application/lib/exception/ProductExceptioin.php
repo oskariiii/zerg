b61ba61dc84e6db227070a2460db9b0e8f70723c
frankie <frankie@bengder.cc>
2020-01-02 13:46:28 +0800

    update 更新报错信息

--- application/lib/exception/ProductExceptioin.php
+++ application/lib/exception/ProductExceptioin.php
@@ -12,5 +12,5 @@ class ProductExceptioin extends BaseException
 {
     public $code        = 404;
     public $errorCode   = 20000;
-    public $msg         = "Sorry喔, 没有找到您指定的Product信息,请查看!";
+    public $msg         = "Sorry喔, 没有找到您指定的商品信息,请查看!";
 }
