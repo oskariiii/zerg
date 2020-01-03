19be77ddcfe7ebc8fe9a44285ac7c36e588a0ccf
frankie <frankie@bengder.cc>
2020-01-03 19:24:37 +0800

    添加新建或更新用户地址控控制器方法, 目前用户与地址为一对一关系, 后续修改为一对多关系

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace app\api\controller\v1;
+
+
+use app\api\validate\AddressNew;
+use app\api\Service\Token as TokenService;
+
+class Address
+{
+    public function createOrUpdateAddress()
+    {
+        # 数据验证
+        (new AddressNew())->goCheck();
+        # 根据token获取uid
+        # 根据uid查找用户数据,判断用户是否存在,如果不存在,返回错误信息. 
+        # 获取用户从客户端传入的地址信息
+        # 判断添加或更新地址
+        # todo 目前地址与用于信息为一对一, 后期修改为一对多
+        $uid = TokenService::getCurrentUid();
+    }
+    
+}
