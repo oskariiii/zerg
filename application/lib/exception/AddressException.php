a7ad3945033679c61430d0939462ec12950ccd9e
frankie <frankie@bengder.cc>
2020-01-06 19:58:35 +0800

    添加地址报错信息

---
+++ application/lib/exception/AddressException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\lib\exception;
+
+
+class AddressException
+{
+    public $code        = 404;
+    public $errorCode   = 40000;
+    public $msg         = "Sorry喔, 没有找到您想找的Banner信息!";
+}
