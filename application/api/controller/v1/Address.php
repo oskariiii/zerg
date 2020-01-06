6d971345db8718a2aef671dd9aa67b906901259b
frankie <frankie@bengder.cc>
2020-01-06 19:57:44 +0800

    添加创建或者更新用户地址方法

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -6,9 +6,14 @@ namespace app\api\controller\v1;
 
 use app\api\validate\AddressNew;
 use app\api\Service\Token as TokenService;
+use app\api\model\User as UserModel;
+use app\lib\exception\SuccessException;
 
 class Address
 {
+    /**
+     * 创建或者更新用户地址(一对一)
+     */
     public function createOrUpdateAddress()
     {
         # 数据验证
@@ -19,6 +24,20 @@ class Address
         # 判断添加或更新地址
         # todo 目前地址与用于信息为一对一, 后期修改为一对多
         $uid = TokenService::getCurrentUid();
+        $user = UserModel::get($uid);
+        if(!user){
+            throw new UserException();
+        }
+        $dataArray = getDatas();
+        $userAddress = $user->address;
+        if(!$userAddress){
+            $user->address()->save($dataArray);
+        }else{
+            # 此处注意 更新 没有 () ****
+            $user->address->save($dataArray);
+        }
+        # return $user;
+        return new SuccessException();
     }
     
 }
