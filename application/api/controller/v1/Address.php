01153fedc988a722767a3a2166dd8590d872feb0
frankie <frankie@bengder.cc>
2020-01-07 11:13:51 +0800

    完善创建/修改用户地址方法, 添加数据验证, 获取数据并提交

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -7,17 +7,20 @@ namespace app\api\controller\v1;
 use app\api\validate\AddressNew;
 use app\api\Service\Token as TokenService;
 use app\api\model\User as UserModel;
-use app\lib\exception\SuccessException;
+use app\lib\exception\SuccessMessage;
 
 class Address
 {
     /**
      * 创建或者更新用户地址(一对一)
+     * @url zerg.cn/api/v1/address
+     * @return SuccessMessage
      */
     public function createOrUpdateAddress()
     {
         # 数据验证
-        (new AddressNew())->goCheck();
+        $validate = new AddressNew();
+        $validate->goCheck();
         # 根据token获取uid
         # 根据uid查找用户数据,判断用户是否存在,如果不存在,返回错误信息. 
         # 获取用户从客户端传入的地址信息
@@ -25,10 +28,11 @@ class Address
         # todo 目前地址与用于信息为一对一, 后期修改为一对多
         $uid = TokenService::getCurrentUid();
         $user = UserModel::get($uid);
-        if(!user){
+        if(!$user){
             throw new UserException();
         }
-        $dataArray = getDatas();
+        # 根据验证器获取参数变量   input('post.') 获取post方式提交的所有参数
+        $dataArray = $validate->getDataByRule(input('post.'));
         $userAddress = $user->address;
         if(!$userAddress){
             $user->address()->save($dataArray);
@@ -37,7 +41,9 @@ class Address
             $user->address->save($dataArray);
         }
         # return $user;
-        return new SuccessException();
+        # 修改请求状态码
+        # post提交数据, 返回状态码为201, 在返回信息时是200, 这里修改一下http请求状态码
+        return json(new SuccessMessage(),201);
     }
     
 }
