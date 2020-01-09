914495fb613927eb7c1e2ed2fe9e14ea4acb63c5
frankie <frankie@bengder.cc>
2020-01-09 10:00:34 +0800

    完成todo目标

--- application/lib/exception/BaseException.php
+++ application/lib/exception/BaseException.php
@@ -32,7 +32,7 @@ class BaseException extends Exception
             # 如果直接return 返回的是默认的成员变量
             # throw new Exception('传入的参数必须是数组');
         }
-        # todo : 这里可以使用foreach循环处理
+        # finished : 这里可以使用foreach循环处理
         # 教学视频中是判断传入的数组中是否存在键值, 存在即修改
         /*if(array_key_exists('code',$param)){
             $this->code = $param['code'];
