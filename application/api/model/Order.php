8e0963b92ab7e19db4e481422c4c7fe764ca2d2d
frankie <frankie@bengder.cc>
2020-01-14 12:58:39 +0800

    添加  自动写入时间戳 : 开启

--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -7,4 +7,7 @@ namespace app\api\model;
 class Order extends BaseModel
 {
     protected $hidden = ['user_id','update_time','delete_time'];
+    protected $autoWriteTimestamp = true;
+    # 自定义时间戳写入字段
+    # protected $createTime = 'create_timestamp';
 }
